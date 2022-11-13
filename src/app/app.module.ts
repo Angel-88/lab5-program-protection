8fd71161bfaeeb04628552ba847695d0e0932522
Angel-88 <dasha.zagrebelna88@gmail.com>
2022-11-13 23:03:06 +0100

    fixed untracked files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CaesarCipherModule } from '../page/caesar-cipher/caesar-cipher.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CaesarCipherModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
